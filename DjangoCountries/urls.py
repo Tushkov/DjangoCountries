ab5e01a23be797f345cb9a2878a2ae00869c77c8
Евгений <Tushkov@neftisa.ru>
2024-06-05 11:20:28 +0300

    initial commit

---
+++ DjangoCountries/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from MainApp import views
+
+urlpatterns = [
+    path('', views.home),
+    path('countries_list', views.countries_list),
+    path('country/<country>', views.country),
+]
---
+++ MainApp/views.py
@@ -0,0 +1,44 @@
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
+from django.http import response
+import json
+from DjangoCountries import settings
+
+file = open(settings.BASE_DIR / "country-by-languages.json")
+data = json.load(file)
+file.close()
+
+             
+# Create your views here.
+def home(request):
+    # text = """
+    # <h1>"Изучаем django День первый !"</h1>
+    # <strong>Автор</strong>: <i>Иванов И.П.</i>
+    # <h2><a href="/about">About</a></h2>
+    # <h2><a href="/items">Items<a></h2>
+    # """
+    # return HttpResponse(text)
+    context = {
+        "name": "Петров Николай Петрович",
+        "email": "my_mail@mail.ru"
+    }
+
+    return render(request, "index.html", context)
+
+def countries_list(request):
+    context = {
+        "items" : data
+    }
+
+    return render(request, "countries_list.html", context)
+
+def country(request, country):
+    for item in data:
+        if country == item["country"]:
+            context = {
+                "item" : item
+            }
+    
+    return render(request, "country.html", context)
+
+
